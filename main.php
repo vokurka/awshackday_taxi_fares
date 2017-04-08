1bf45d0cd87a19e3bbbcd13c5efde19da2032f04
Vojtech Kurka <vokurka@keboola.com>
2017-04-08 16:47:05 +0800

    fix

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 use Keboola\Json\Parser;
 
-define('OUT_PATH', './out/');
+define('OUT_PATH', '/data/out/tables/');
 
 $configFile = getenv('KBC_DATADIR') . DIRECTORY_SEPARATOR . 'config.json';
 $config = json_decode(file_get_contents($configFile), true);
@@ -11,12 +11,18 @@ $config = json_decode(file_get_contents($configFile), true);
 $response = file_get_contents($config['parameters']['url']);
 $response = json_decode($response);
 
+if ($response === NULL)
+{
+	echo "Bad JSON.\n";
+	exit;
+}
+
 // Create parser and parse the json
 $parser = Parser::create(new \Monolog\Logger('json-parser'));
-$parser->process($json, $entityName);
+$parser->process($response->directions->modes->taxi->routes, 'taxi');
 $result = $parser->getCsvFiles();
 
 foreach ($result as $f)
 {
-	mergeCsvFiles($f->getPathName(), OUT_PATH.$config['parameters']['output_bucket'].".". substr($f->getFileName(), strpos($f->getFileName(), '-')+1));
+	copy($f->getPathName(), OUT_PATH.$config['parameters']['output_bucket'].".". substr($f->getFileName(), strpos($f->getFileName(), '-')+1));
 }
